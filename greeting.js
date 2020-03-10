df88b960f4149ae29fe2ad247e3e41633da8aefd
grace <jailke93@gmail.com>
2020-03-10 12:18:58 +0900

    error

--- greeting.js
+++ greeting.js
@@ -2,15 +2,15 @@ const form = document.querySelector(".js-form"),
   input = form.querySelector("input"),
   greeting = document.querySelector(".js-greetings");
 
-const USER_LS = "currentUser", // User Local Storage
-  SHOWING_CN = "showing"; // class Name = "showing"
+const USER_LS = "currentUser",
+  SHOWING_CN = "showing";
 
 function saveName(text) {
-  localStorage.setItem(USER_LS, text); // 입력한 이름을 저장한다. local storage는 URL을 기반으로 동작
+  localStorage.setItem(USER_LS, text);
 }
 
 function handleSubmit(event) {
-  event.preventDefault(); // Default Behavior를 막는다. (enter 치면 이름이 사라지는 event)
+  event.preventDefault();
   const currentValue = input.value;
   paintGreeting(currentValue);
   saveName(currentValue);
@@ -22,7 +22,6 @@ function askForName() {
 }
 
 function paintGreeting(text) {
-  // currentUser가 있다면 그 사람의 이름을 색칠해보자!
   form.classList.remove(SHOWING_CN);
   greeting.classList.add(SHOWING_CN);
   greeting.innerText = `Hello ${text}`;
